e431905563de8f3387d6d9a8709c87cbc3f4a4b3
Bàn Hà Tân <bhtan.game@gmail.com>
2026-01-15 17:25:00 +0700

    Player Controller
    Object visual tips
    Input basic
    Access Modifier
    Move Direction
    Serialized field
    Time.deltaTime -> no FPS depend
    How to move character  with speed, direction and time adjust

---
+++ Assets/_Script/Player.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+        // good code, no Z Axis if the game is not allowed
+        if(Keyboard.current.wKey.isPressed)
+        {
+            inputVector.y += 1;
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            inputVector.y -= 1;
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            inputVector.x -= 1;
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            inputVector.x += 1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime; 
+        Debug.Log(Time.deltaTime);
+
+    }
+}
